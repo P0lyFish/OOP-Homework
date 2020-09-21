09dad3f4524947fe104feca22f4e41ee2105f030
P0lyFish <tranthephong32@gmail.com>
2020-09-22 00:45:49 +0700

    add word pronunciation

--- src/backend/Word.java
+++ src/backend/Word.java
@@ -4,12 +4,23 @@ package backend;
 public class Word {
     private String word_target;
     private String word_explain;
+    private String word_pronunciation;
 
-    Word() {}
+    Word() {
+        word_target = "no data";
+        word_explain = "no data";
+        word_pronunciation = "no data";
+    }
 
     Word(String word_target, String word_explain) {
         this.word_target = word_target;
         this.word_explain = word_explain;
+    }
+
+    Word(String word_target, String word_explain, String word_pronunciation) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+        this.word_pronunciation = word_pronunciation;
     }
 
     public void setWordTarget(String word_target) {
@@ -26,5 +37,13 @@ public class Word {
 
     public String getWordExplain() {
         return word_explain;
+    }
+
+    public void setWordPronunciation(String word_pronunciation) {
+        this.word_pronunciation = word_pronunciation;
+    }
+
+    public String getWordPronunciation() {
+        return word_pronunciation;
     }
 }
